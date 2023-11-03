02bccffccfbe29cc4239f6818c65cbe80bbe37eb
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-11-03 11:59:39 +0530

    update url and delete cypress config

--- cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const codeCoverageTask = require('@cypress/code-coverage/task');
-
-module.exports = (on, config) => {
-  codeCoverageTask(on, config);
-  // other plugins, if any
-  return config;
-};
