f11f577afcb610403aa06b38152af3486cd4ef15
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-11-07 16:46:39 +0530

    include unique project is

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,30 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
-module.exports = defineConfig(
-  {
+module.exports = defineConfig({
+  projectId: "7qguxw",
   e2e: {
-    setupNodeEvents(on,config){
+    setupNodeEvents(on, config) {
       return config;
     },
-    baseUrl: 'http://localhost:8001',
-    specPattern : "cypress/e2e/**/*spec.js",
+    baseUrl: "http://localhost:8001",
+    specPattern: "cypress/e2e/**/*spec.js",
     nodeVersion: "system",
     screenshotOnRunFailure: false,
     chromeWebSecurity: false,
     video: false,
     reporter: "cypress-mochawesome-reporter",
     reporterOptions: {
-        reportDir: "cypress/reports",
-        charts: true,
-        quite: true,
-        overwrite: false,
-        html: false,
-        json: true,
-      },
+      reportDir: "cypress/reports",
+      charts: true,
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
   },
   env: {
     codeCoverage: {
       url: "http://localhost:3000/api/__coverage__",
     },
-  }
-})
+  },
+});
